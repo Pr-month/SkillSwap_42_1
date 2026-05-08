47ad9debc0a080be818814dfdd692c0c4b6caf5d
Dmitry Popov <dmitrytga@yandex.ru>
2026-05-09 02:33:15 +0300

    feat: описан конфиг приложения c интеграцией в bootstrap, функция хеша пароля

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { InjectRepository } from '@nestjs/typeorm';
+import * as bcrypt from 'bcrypt';
 import { Repository } from 'typeorm';
 import { User } from '../users/entities/user.entity';
 import { CreateAuthDto } from './dto/create-auth.dto';
@@ -10,7 +12,18 @@ export class AuthService {
   constructor(
     @InjectRepository(User)
     private readonly usersRepository: Repository<User>,
+    private readonly configService: ConfigService,
   ) {}
+
+  /** Хеширует пароль с помощью bcrypt.
+   * 
+   * @param plainPassword - Пароль, введенный пользователем.
+   * @returns Хеш пароля.
+   */
+  async hashPassword(plainPassword: string): Promise<string> {
+    const rounds = this.configService.get<number>('app.hashSalt') ?? 10;
+    return bcrypt.hash(plainPassword, rounds);
+  }
 
   create(createAuthDto: CreateAuthDto) {
     return {
