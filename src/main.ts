70a4e822b763b5fef0d8f7abf11d2174d1c89c56
Damir Aziev <hideru.a-dam@yandex.com>
2026-05-20 04:56:19 +0500

    Revert "feat(files): add file upload module with multer and static serving"
    This reverts commit 6354acfdf7ac19cefa9119df5196f95bb33c2497.

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,12 @@
 import { ConfigService } from '@nestjs/config';
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-import { NestExpressApplication } from '@nestjs/platform-express';
 import * as cookieParser from 'cookie-parser';
-import { join } from 'path';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  const app = await NestFactory.create(AppModule);
   app.use(cookieParser());
-  app.useStaticAssets(join(process.cwd(), 'public'));
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
