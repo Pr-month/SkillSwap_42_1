a0f0a6fa5097d8a490f70c866615a88f80abc95f
angeel2 <angelin4vagena2016.ru@yandex.ru>
2026-05-27 22:19:32 +0300

    feat: add POST /requests endpoint

--- src/requests/requests.service.ts
+++ src/requests/requests.service.ts
@@ -1,19 +1,46 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import {
+  Injectable,
+  NotFoundException,
+  ForbiddenException,
+  BadRequestException,
+} from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { CreateRequestDto } from './dto/create-request.dto';
 import { UpdateRequestDto } from './dto/update-request.dto';
 import { Request } from './entities/request.entity';
+import { SkillsService } from '../skills/skills.service';
+import { RequestStatus } from './enums/request-status.enum';
 
 @Injectable()
 export class RequestsService {
   constructor(
     @InjectRepository(Request)
     private readonly requestsRepository: Repository<Request>,
+    private readonly skillsService: SkillsService,
   ) {}
 
-  async create(createRequestDto: CreateRequestDto) {
-    const request = this.requestsRepository.create(createRequestDto);
+  async create(createRequestDto: CreateRequestDto, senderId: number) {
+    const { offeredSkillId, requestedSkillId } = createRequestDto;
+    const offeredSkill = await this.skillsService.findOne(offeredSkillId);
+    if (offeredSkill.owner.id !== senderId) {
+      throw new ForbiddenException('You can only offer your own skills');
+    }
+    const requestedSkill = await this.skillsService.findOne(requestedSkillId);
+    const receiverId = requestedSkill.owner.id;
+    if (senderId === receiverId) {
+      throw new BadRequestException(
+        'You cannot request an exchange with yourself',
+      );
+    }
+    const request = this.requestsRepository.create({
+      sender: { id: senderId },
+      receiver: { id: receiverId },
+      offeredSkill,
+      requestedSkill,
+      status: RequestStatus.PENDING,
+    });
+
     return this.requestsRepository.save(request);
   }
 
