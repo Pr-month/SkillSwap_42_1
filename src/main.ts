dda81f84d35f2a9bbd8a66f550f535e6ef44926e
Dmitry Popov <dmitrytga@yandex.ru>
2026-06-02 20:02:32 +0300

    feat: подключен Swagger в main.ts, добавлена пропущенная запятая в package.json

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { ConfigService } from '@nestjs/config';
 import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
 import { NestFactory, Reflector } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import * as cookieParser from 'cookie-parser';
 import { join } from 'path';
 import { AppModule } from './app.module';
@@ -20,6 +21,16 @@ async function bootstrap() {
     }),
   );
   app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
+  app.setGlobalPrefix('api');
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('SkillSwap API')
+    .setDescription('Документация REST API сервиса SkillSwap')
+    .setVersion('0.0.1')
+    .build();
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('docs', app, document, { useGlobalPrefix: true });
+
   const configService = app.get(ConfigService);
   const port = configService.get<number>('APP_CONFIG.port') ?? 3000;
   await app.listen(port);
